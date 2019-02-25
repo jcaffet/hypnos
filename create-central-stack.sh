cf9020c6ff1b7d237fdffc2e15ad3da48a06b589
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-02-25 22:23:16 +0100

    improve multi-account usage

--- create-central-stack.sh
+++ create-central-stack.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 usage(){
-    echo "Usage: $0 <profile>" 
+    echo "Usage: $0 <profile> <environment>" 
     echo "profile : aws profile to use for deployment" 
+    echo "environment : environment suffix when using Hypnos twice in the same account" 
 }
 
-if [ $# -eq 1 ]; then
+if [ $# -eq 2 ]; then
    profile=$1
+   environment=$2
 else
    usage;
    exit 1;
@@ -15,12 +17,13 @@ fi
 echo "Zipping source"
 zip hypnos-terminate.py.zip hypnos-terminate.py
 echo "Copying source"
-HYPNOS_BUCKET=operations-services-corp
-aws s3 cp hypnos-terminate.py.zip s3://${HYPNOS_BUCKET}/hypnos/
+HYPNOS_BUCKET=hypnos-543476789297
+aws --profile=${profile} s3 cp hypnos-terminate.py.zip s3://${HYPNOS_BUCKET}/source/
 echo "Creating stack"
-aws cloudformation create-stack \
-    --stack-name operations-hypnos-central-${profile} \
+aws --profile=${profile} cloudformation create-stack \
+    --stack-name hypnos-central \
     --capabilities CAPABILITY_NAMED_IAM \
     --template-body file://cf-hypnos-central.yml \
-    --parameters ParameterKey=Environment,ParameterValue=${profile}
+    --parameters ParameterKey=Environment,ParameterValue=${environment} \
+    --parameters ParameterKey=LambdaS3Bucket,ParameterValue=${HYPNOS_BUCKET}
 
--- create-spoke-stack.sh
+++ create-spoke-stack.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 usage(){
-    echo "Usage: $0 <profile>" 
+    echo "Usage: $0 <profile> <central-account>" 
     echo "profile : aws profile to use for deployment" 
+    echo "central-account : aws profile where Hypnos is runs" 
 }
 
-if [ $# -eq 1 ]; then
+if [ $# -eq 2 ]; then
    profile=$1
+   central_account=$2
 else
    usage;
    exit 1;
@@ -14,7 +16,8 @@ fi
 
 echo "Creating stack"
 aws --profile=${profile} cloudformation create-stack \
-    --stack-name hypnos-spoke \
+    --stack-name hypnos-spoke-account-role \
     --capabilities CAPABILITY_NAMED_IAM \
-    --template-body file://cf-hypnos-spoke.yml
+    --template-body file://cf-hypnos-spoke.yml \
+    --parameters ParameterKey=CentralHypnosAccount,ParameterValue=${central_account}
 
